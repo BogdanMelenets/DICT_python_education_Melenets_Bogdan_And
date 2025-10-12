2185859f64567445bcaea18423600b2c25d253d8
BogdanMelenets <b.a.melenets@student.khai.edu>
2025-10-12 12:55:25 +0300

    ChatBot 5-th stage

---
+++ ChatBot/chatbot.py
@@ -0,0 +1,36 @@
+print ("Hello! My name is My_bot_1st.")
+print ("I was created in 2025.")
+print ("Please, remind me your name.")
+name = input (">")
+print ("What a great name you have, ", name)
+print ("Let me guess your age.")
+print ("Enter remainders of dividing your age by 3, 5 and 7.")
+remainder3 = int(input(">"))
+remainder5 = int(input(">"))
+remainder7 = int(input(">"))
+age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+print ("Your age is", age, " that's a good time to start programming!")
+print ("Now I will prove to you that I can count to any number you want.")
+co = int (input (">"))
+i = 0
+while i < co+1:
+       print(i,"!")
+       i = i + 1
+print ("Completed, have a nice day!")
+print ("Let's test your programming knowledge.")
+print ("Why do we use methods?")
+print ("1. To repeat a statement multiple times.")
+print ("2. To decompose a program into several small subroutines.")
+print ("3. To determine the execution time of a program.")
+print ("4. To interrupt the execution of a program.")
+res = 0
+vidpovid = int(input(">"))
+while res < 1 :
+      if vidpovid == 2 :
+          res = 1
+          print("Completed, have a nice day!")
+          print("Congratulations, have a nice day!")
+      else:
+          print("Please, try again.")
+          vidpovid = int(input(">"))
+
