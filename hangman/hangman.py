5b5253ebd40517a7f332e9980acdbbffa586c7fe
BogdanMelenets <b.a.melenets@student.khai.edu>
2025-10-25 18:00:28 +0300

    4-th

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -3,6 +3,13 @@ print ("HANGMAN")
 print ("The game will be available soon.")
 words=["python", "java", "javascript", "php"]
 testword = random.choice (words)
-word=input ("Guess the word: >")
+str=("Guess the word: "+testword[:3])
+len1 = len(testword)
+i=0
+while i<(len1-3):
+    str = str + "-"
+    i=i+1
+str=str+">"
+word=input (str)
 if word==testword: print ("You survived!")
 else: print ("You lost!")
