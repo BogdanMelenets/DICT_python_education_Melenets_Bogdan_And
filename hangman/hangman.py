59d79c3bf314e18e3d87d965974b482772a45065
BogdanMelenets <b.a.melenets@student.khai.edu>
2025-10-25 20:15:37 +0300

    7-th

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -19,24 +19,27 @@ while sproba<8:
    print("Спроба №", sproba)
    word=input ("Input a letter: >")
    posspr=wordspr.find(word[0])
-   if  posspr<0:
-      wordspr = wordspr+word[0]
-      Pos=testword.find(word)
-      if  Pos>-1:
-          j=Pos
-          strnew=(strnew[:j]+word[0]+strnew[j+1:])
-          j=j+1
-          while j<(len1):
-             if testword[j]==word[0]:  strnew=(strnew[:j]+word[0])
-             else: strnew=(strnew[:j+1]+"-")
-             j = j + 1
-          strnew = (strnew[:len1])
-          print(strnew)
-      else: sproba = sproba + 1;  print("That letter doesn't appear in the word")
+   if len(word)<2:
+      if (word.islower()):
+         if  posspr<0:
+            wordspr = wordspr+word[0]
+            Pos=testword.find(word)
+            if  Pos>-1:
+                j=Pos
+                strnew=(strnew[:j]+word[0]+strnew[j+1:])
+                j=j+1
+                while j<(len1):
+                   if testword[j]==word[0]:  strnew=(strnew[:j]+word[0])
+                   else: strnew=(strnew[:j+1]+"-")
+                   j = j + 1
+                strnew = (strnew[:len1])
+                print(strnew)
+            else: sproba = sproba + 1;  print("That letter doesn't appear in the word")
 
+         else: print("You've already guessed this letter.")
+      else: print ("Please enter a lowercase English letter.")
    else:
-       sproba = sproba + 1
-       print("No improvements")
+       print("You should input a single letter.")
 print(strnew)
 if strnew==testword: print ("You survived!")
 else: print("Thanks for playing! We'll see how well you did in the next stage")
