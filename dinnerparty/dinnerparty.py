aa3343237db4e000b3514ef279435b24cf4f4809
BogdanMelenets <b.a.melenets@student.khai.edu>
2025-11-02 19:46:48 +0200

    4-st

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -1,7 +1,7 @@
 import random
 print("Enter the number of friends joining (including you):")
 coun=input (">")
-count = int(coun)
+count = int(coun)-1
 friends= {}
 if count > 0:
     print("Enter the name of every friend (including you), each on a new line:")
@@ -25,10 +25,18 @@ if count > 0:
     if luc=="Yes":
         ii=random.randint(0, count-1)
         jj=0
+        newsumone = round(sum / (count), 2)
+        lucky=""
         for Fr, value in friends.items():
             if jj==ii:
-                 print(f'{Fr}', "is the lucky one!")
+                 lucky=f'{Fr}'
+                 print(lucky, "is the lucky one!")
             jj=jj+1
+        for i1 in friends:
+            friends[i1] = newsumone
+        friends[lucky] = 0
+        for Fr, value in friends.items():
+            print(f'{Fr}: {value}')
     else:
         print("No one is going to be lucky.")
 else:
