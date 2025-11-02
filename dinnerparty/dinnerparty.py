d83a2ef5b18b514b22a02eeb1f5045b41b29e46a
BogdanMelenets <b.a.melenets@student.khai.edu>
2025-11-02 18:38:58 +0200

    2-rd

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -1,14 +1,22 @@
 print("Enter the number of friends joining (including you):")
 coun=input (">")
 count = int(coun)
-friends= {0: ""}
+friends= {}
 if count > 0:
     print("Enter the name of every friend (including you), each on a new line:")
     i=0
     while i < count:
-        friends[i]=input(">")
+        fr=input(">")
+        friends.update({fr :0})
         i=i+1
+    print(friends.items())
+    print("Enter the total amount")
+    su = input(">")
+    sum = float(su)
+    sumone=round(sum/(count + 1), 2)
     for i in friends:
-        print(friends[i])
+        friends[i]=sumone
+    print(friends.items())
+
 else:
     print("No one is joining for the party")
