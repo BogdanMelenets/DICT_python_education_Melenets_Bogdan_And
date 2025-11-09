3f67f7b87f3eedeb707172e5d94ed828b650ff16
BogdanMelenets <b.a.melenets@student.khai.edu>
2025-11-09 18:09:06 +0200

    2-nd

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -1,6 +1,20 @@
-Pole=[['X', '0', '0'], ['0', 'X', '0'], ['0', 'X', 'X']]
-
-for i in range(len(Pole)):
-    str=""
-    for j in range(len(Pole)): str=str+Pole[i][j]
-    print (str)
+Pole=[['-', '-', '-', '-', '-'], ['|', 'X', '0', '0', '|'], ['|', '0', 'X', '0', '|'], ['|', '0', 'X', 'X', '|'], ['-', '-', '-', '-', '-']]
+inpole=input("Enter cells:")
+inm=1
+for i in range(len(inpole)):
+    if inpole[i] not in ['0', 'X', '_', '-', '|']: inm=0
+if (inm==1 and (len(inpole)==9)):
+    Pole[1][1]=inpole[0]
+    Pole[1][2] = inpole[1]
+    Pole[1][3] = inpole[2]
+    Pole[2][1] = inpole[3]
+    Pole[2][2] = inpole[4]
+    Pole[2][3] = inpole[5]
+    Pole[3][1] = inpole[6]
+    Pole[3][2] = inpole[7]
+    Pole[3][3] = inpole[8]
+    for i in range(len(Pole)):
+       str=""
+       for j in range(len(Pole)): str=str+Pole[i][j]
+       print (str)
+else: print ("Error")
