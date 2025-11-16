6c97f6465dada7ebe2c174773462e493f34c663c
BogdanMelenets <b.a.melenets@student.khai.edu>
2025-11-16 20:55:04 +0200

    4-th

--- pencils_game/pencils_game.py
+++ pencils_game/pencils_game.py
@@ -1,3 +1,4 @@
+import random
 print("How many pencils would you like to use:")
 err=0
 ccells=0
@@ -27,20 +28,35 @@ for i in range(ccells): cells=cells+"|"
 print(cells)
 print(ncells, "is going first!")
 zalishok=ccells
-if ncells==name[0]: first=0
-else: first=1
+if ncells==name[0]:
+    first=0
+    bot=1
+else:
+    first=1
+    bot=0
 while zalishok>0:
    print(name[first], "turn:")
    err = 0
    while err == 0:
-       minusstr = input(">")
-       if minusstr not in ["1", "2", "3"]: print("Possible values: '1', '2' or '3'")
+       if first != bot :
+          minusstr = input(">")
+          if minusstr not in ["1", "2", "3"]: print("Possible values: '1', '2' or '3'")
+          else:
+             minus = int(minusstr)
+             err=1
+             if minus > zalishok:
+                 err=0
+                 print("Too many pencils were taken")
        else:
-          minus = int(minusstr)
-          err=1
-          if minus > zalishok:
-              err=0
-              print("Too many pencils were taken")
+           err=1
+           if zalishok > 25:  minus=random.randint(1, 3)
+           else:
+              if zalishok in [4, 8, 12, 16, 20, 24]: minus=3
+              if zalishok in [3, 7, 11, 15, 19, 23]: minus = 2
+              if zalishok in [2, 6, 10, 14, 18, 22]: minus = 1
+              if zalishok==1:  minus=1
+              if zalishok in [5, 9, 13, 17, 21, 25]: minus=random.randint(1, 3)
+           print (minus)
    zalishok=zalishok-minus
    cells = ""
    for i in range(zalishok): cells = cells + "|"
