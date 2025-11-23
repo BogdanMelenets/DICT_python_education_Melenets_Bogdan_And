5e8745ecdd458672ab68b9a410549bad96146f21
BogdanMelenets <b.a.melenets@student.khai.edu>
2025-11-23 12:30:23 +0200

    2-nd

--- coffeemachine/coffeemachine.py
+++ coffeemachine/coffeemachine.py
@@ -5,3 +5,12 @@ print("Mixing boiled water with crushed coffee beans")
 print("Pouring coffee into the cup")
 print("Pouring some milk into the cup")
 print("Coffee is ready!")
+cou=int(input("Write how many cups of coffee you will need:"))
+water=cou*200
+milk=cou*50
+beans=cou*15
+print("For", cou, "cups of coffee you will need:")
+print (water, "ml of water")
+print (milk, "ml of milk")
+print (beans, "g of coffee beans")
+
