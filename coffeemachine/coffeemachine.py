6671bb7fe4b4d139b44d081183f6916996ff9907
BogdanMelenets <b.a.melenets@student.khai.edu>
2025-11-23 19:17:01 +0200

    4-th

--- coffeemachine/coffeemachine.py
+++ coffeemachine/coffeemachine.py
@@ -1,42 +1,58 @@
-print("Starting to make a coffee")
-print("Grinding coffee beans")
-print("Boiling water")
-print("Mixing boiled water with crushed coffee beans")
-print("Pouring coffee into the cup")
-print("Pouring some milk into the cup")
-print("Coffee is ready!")
-mashin=[0, 0, 0]
-#water, milk, beans
-mashin[0]=int(input("Write how many ml of water the coffee machine has:"))
-mashin[1]=int(input("Write how many ml of milk the coffee machine has:"))
-mashin[2]=int(input("Write how many grams of coffee beans the coffee machine has:"))
+def buy (mashin):
+    # water, milk, beans, $
+   cup=[[250, 0, 16, 4], [350, 75, 20, 7], [200, 100, 12, 6]]
+   print ("1> еспресо")
+   print ("2> лате")
+   print ("3> капучіно")
+   v=int(input("Enter your choice>"))-1
+   if mashin[0] >= cup[v][0] and mashin[1] >= cup[v][1] and mashin[2] >= cup[v][2] and mashin[4] >0:
+       mashin[0] = mashin[0] - cup[v][0]
+       mashin[1] = mashin[1] - cup[v][1]
+       mashin[2] = mashin[2] - cup[v][2]
+       mashin[3] = mashin[3] + cup[v][3]
+       mashin[4] = mashin[4] - 1
+   return mashin
 
-cou=int(input("Write how many cups of coffee you will need:"))
-water=200*cou
-milk=50*cou
-beans=15*cou
-countcup=[0, 0, 0]
-#water, milk, beans
-if mashin[0]>=water and mashin[1]>=milk and mashin[2]>=beans:
-    print ("Yes, I can make that amount of coffee.")
-    if mashin[0] > water and mashin[1] > milk and mashin[2] > beans:
-        countcup[0] = int(round((mashin[0]-water)/200,0))
-        countcup[1]= int(round((mashin[1]-milk)/50, 0))
-        countcup[2]= int(round((mashin[2]-beans)/15, 0))
-        countcup.sort()
-        print ("Yes, I can make that amount of coffee (and even", countcup[0], "more than that)")
-if mashin[0]<water or mashin[1]<milk or mashin[2]<beans:
-    countcup[0] = int(round(mashin[0] / 200, 0))
-    countcup[1] = int(round(mashin[1] / 50, 0))
-    countcup[2] = int(round(mashin[2] / 15, 0))
-    countcup.sort()
-    print("No, I can make only", countcup[0], "cups of coffee")
+def fill (mashin):
+    print("Write how many ml of water you want to add:")
+    mashin[0]=mashin[0]+int(input(">"))
+    print("Write how many ml of milk you want to add:")
+    mashin[1] = mashin[1] + int(input(">"))
+    print("Write how many grams of coffee beans you want to add:")
+    mashin[2] = mashin[2] + int(input(">"))
+    print("Write how many disposable coffee cups you want to add:")
+    mashin[4] = mashin[4] + int(input(">"))
+    return mashin
+
+def take (mashin):
+    mashin[3]=0
+    return mashin
+
+# water, milk, beans, $, cups
+mashin=[400, 540, 120, 550, 9]
+print("The coffee machine has:")
+print(mashin[0], "of water")
+print(mashin[1], " of milk")
+print(mashin[2], " of coffee beans")
+print(mashin[4], "of disposable cups")
+print(mashin[3], " of money")
+
+action=input("Write action (buy, fill, take):")
+if action=="buy": mashin = buy (mashin)
+if action=="fill": mashin = fill (mashin)
+if action=="take": mashin = take (mashin)
+
+
+print("The coffee machine has:")
+print(mashin[0], "of water")
+print(mashin[1], " of milk")
+print(mashin[2], " of coffee beans")
+print(mashin[4], "of disposable cups")
+print(mashin[3], " of money")
 
 
 
-print("For", cou, "cups of coffee you will need:")
-print (water, "ml of water")
-print (milk, "ml of milk")
-print (beans, "g of coffee beans")
+
+
 
 
