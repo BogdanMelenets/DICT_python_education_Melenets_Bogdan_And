b085d0db583c202cf18213b1332248067f19447c
BogdanMelenets <b.a.melenets@student.khai.edu>
2025-12-08 17:14:02 +0200

    2-nd

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -2,3 +2,10 @@ def print_static_grid():
     print("X O X")
     print("O X O")
     print("X X O")
+def print_grid(cells):
+    # Друк рамки
+    print("---------")
+    # Виводимо 3 рядки по 3 символи
+    for i in range(0, 9, 3):
+        print("|", cells[i], cells[i+1], cells[i+2], "|")
+    print("---------")
