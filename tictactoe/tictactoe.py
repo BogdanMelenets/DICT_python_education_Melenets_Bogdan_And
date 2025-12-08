06ba27d0ba560057d1faf2ac52e895a039d8f5d3
BogdanMelenets <b.a.melenets@student.khai.edu>
2025-12-08 17:16:28 +0200

    4-th

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -2,6 +2,7 @@ def print_static_grid():
     print("X O X")
     print("O X O")
     print("X X O")
+
 def print_grid(cells):
     # Друк рамки
     print("---------")
@@ -9,6 +10,7 @@ def print_grid(cells):
     for i in range(0, 9, 3):
         print("|", cells[i], cells[i+1], cells[i+2], "|")
     print("---------")
+
 def check_win(cells):
     wins = [
         (0, 1, 2), (3, 4, 5), (6, 7, 8),      # рядки
@@ -43,3 +45,31 @@ def check_win(cells):
     if "_" in cells:
         return "Game not finished"
     return "Draw"
+
+def user_move(cells):
+    while True:
+        coords = input("Enter the coordinates: ").split()
+
+        # Перевірка на числа
+        if not all(c.isdigit() for c in coords):
+            print("You should enter numbers!")
+            continue
+
+        x, y = map(int, coords)
+
+        # Перевірка діапазону
+        if x not in [1, 2, 3] or y not in [1, 2, 3]:
+            print("Coordinates should be from 1 to 3!")
+            continue
+
+        # Перетворення координат в індекс
+        index = (x - 1) * 3 + (y - 1)
+
+        # Перевірка зайнятості
+        if cells[index] != "_":
+            print("This cell is occupied! Choose another one!")
+            continue
+
+        # Робимо хід
+        cells[index] = "X"
+        return cells
