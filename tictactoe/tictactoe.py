bdeed2e23fa0775508a684e48bccaf8c227102dc
BogdanMelenets <b.a.melenets@student.khai.edu>
2025-12-11 17:33:07 +0200

    2-nd

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -4,3 +4,11 @@ def print_grid(grid):
     for row in grid:
         print("|", " ".join(row), "|")
     print("---------")
+
+def string_to_grid(cells):
+    grid = [
+        [cells[0], cells[1], cells[2]],
+        [cells[3], cells[4], cells[5]],
+        [cells[6], cells[7], cells[8]]
+    ]
+    return grid
