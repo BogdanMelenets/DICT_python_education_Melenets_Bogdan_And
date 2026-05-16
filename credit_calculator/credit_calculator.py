acf1746c7dd5ac1381b18f42cc524c972912a80c
BogdanMelenets <b.a.melenets@student.khai.edu>
2026-05-16 14:10:40 +0300

    3-rd

--- credit_calculator/credit_calculator.py
+++ credit_calculator/credit_calculator.py
@@ -1,3 +1,5 @@
+import math
+"""
 def rozrah(Sum, Type):
     result="Error"
     if Type == 'm':
@@ -16,3 +18,50 @@ print("What do you want to calculate?")
 print('type "m" – for number of monthly payments,')
 print('type "p" – for the monthly payment:')
 print(rozrah(pochsum, input('>')))
+"""
+
+def rozrah(Type):
+    result="Error"
+    if Type == 'n':
+        print("Enter the loan principal:")
+        Sum=int(input(">"))
+        print("Enter the monthly payment:")
+        Monthpay = int(input(">"))
+        print("Enter the loan interest:")
+        Loan = float(input(">"))
+        NomLoan=(Loan/(12*100))
+        MontsCount=round(math.log(Monthpay/(Monthpay-NomLoan*Sum), (1+NomLoan))+0.5)
+        YearCount=round(MontsCount/12-0.5)
+        LastMonthCount=MontsCount-YearCount*12
+        result = "It will take "
+        if YearCount>0: result= result+str(YearCount)+" years"
+        if LastMonthCount>0:
+            if YearCount > 0: result = result + " and "
+            result=result + str(LastMonthCount)+ " months to repay this loan!"
+    if Type=='a':
+        print("Enter the loan principal:")
+        Sum = int(input(">"))
+        print("Enter the number of periods:")
+        MonthCount = int(input(">"))
+        print("Enter the loan interest:")
+        Loan = float(input(">"))
+        NomLoan = (Loan / (12 * 100))
+        MonthPay=(Sum*(NomLoan*math.pow((1+NomLoan), MonthCount)))/(math.pow((1+NomLoan),MonthCount) -1)
+        result= "Your monthly payment = "+str(round(MonthPay+0.5))+"!"
+    if Type == 'p':
+        print("Enter the annuity payment:")
+        MonthPay = float(input(">"))
+        print("Enter the number of periods:")
+        MonthCount = int(input(">"))
+        print("Enter the loan interest:")
+        Loan = float(input(">"))
+        NomLoan = (Loan / (12 * 100))
+        Sum = MonthPay / ((NomLoan * math.pow((1 + NomLoan), MonthCount))/ (math.pow((1 + NomLoan), MonthCount) - 1))
+        result="Your loan principal ="+str (round(Sum))+"!"
+    return result
+
+print("What do you want to calculate?")
+print('type "n" for number of monthly payments,')
+print('type "a" for annuity monthly payment amount,')
+print('type "p" for loan principal:')
+print(rozrah(input('>')))
