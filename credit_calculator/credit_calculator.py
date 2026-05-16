3dae8733ea9d2c854faa6dc793beff89fea41b4a
BogdanMelenets <b.a.melenets@student.khai.edu>
2026-05-16 13:12:36 +0300

    2-nd

--- credit_calculator/credit_calculator.py
+++ credit_calculator/credit_calculator.py
@@ -1,5 +1,18 @@
-print("Loan principal: 1000")
-print("Month 1: repaid 250")
-print("Month 2: repaid 250")
-print("Month 3: repaid 500")
-print("The loan has been repaid!")
+def rozrah(Sum, Type):
+    result="Error"
+    if Type == 'm':
+        paymonth=int(input("Enter the monthly payment:"))
+        result="It will take "+str(round(Sum/paymonth)+0.5)+" months to repay the loan"
+    if Type == 'p':
+        paymonth = int(input("Enter the number of months:"))
+        payment=round(Sum/(paymonth)+0.5)
+        lastpayment = Sum-(paymonth-1) * payment
+        result="Your monthly payment = "+ str(payment) +" and the last payment = "+ str(lastpayment)
+    return result
+
+
+pochsum = int(input("Enter the loan principal:"))
+print("What do you want to calculate?")
+print('type "m" – for number of monthly payments,')
+print('type "p" – for the monthly payment:')
+print(rozrah(pochsum, input('>')))
