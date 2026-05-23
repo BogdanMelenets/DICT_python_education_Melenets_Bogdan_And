7c28c3810e33b26e71009173e40d131eda72ac10
BogdanMelenets <b.a.melenets@student.khai.edu>
2026-05-23 15:40:13 +0300

    3-rd

--- web_page_scraper/web_page_scraper.py
+++ web_page_scraper/web_page_scraper.py
@@ -12,37 +12,41 @@ url = input("Input the URL:")
 
 try:
     response = requests.get(url, timeout=5) # timeout задає час очікування у секундах
-
+    page_content = requests.get(url).content
     #response = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'}, timeout=5)
     response.raise_for_status() # Викидає помилку для кодів 4xx або 5xx
     print("Дані успішно отримано:")
+
 
     # print("Дані із json")
     try:
        # data = response.json()
        # print("Цитати: ", data["quote"])
+        with open("source.html", "wb") as file:
+           file.write(response.content)
+        print("Content saved.")
 
-        soup = BeautifulSoup(response.text, "html.parser")
+       # soup = BeautifulSoup(response.text, "html.parser")
 
         # Назва фільму з тегу <title>
-        title_tag = soup.find("title")
-        movie_title = (
-            title_tag.text.strip() if title_tag else "Invalid quote resource!"
-        )
+        #title_tag = soup.find("title")
+        #movie_title = (
+            #title_tag.text.strip() if title_tag else "Invalid quote resource!"
+        #)
 
         # Опис фільму в тегу <meta name="description">
-        meta_description_tag = soup.find("meta", {"name": "description"})
+        # meta_description_tag = soup.find("meta", {"name": "description"})
 
         # Отримуємо доступ через ['content'], якщо тег існує
-        if meta_description_tag and "content" in meta_description_tag.attrs:
-            movie_description = meta_description_tag["content"].strip()
-        else:
-            movie_description = "Invalid quote resource!"
+        # if meta_description_tag and "content" in meta_description_tag.attrs:
+          #  movie_description = meta_description_tag["content"].strip()
+        # else:
+          #  movie_description = "Invalid quote resource!"
 
         # Формуємо та повертаємо підсумковий словник
-        movie_data = {"Назва": movie_title, "Опис": movie_description}
+        # movie_data = {"Назва": movie_title, "Опис": movie_description}
 
-        print (movie_data)
+        # print (movie_data)
     except requests.exceptions.JSONDecodeError:
          if response.status_code == 200:
            print("Response was not JSON")
