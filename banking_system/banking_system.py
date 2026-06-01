fbf0ad2107d969b76a33111534807cbbb989db14
BogdanMelenets <b.a.melenets@student.khai.edu>
2026-06-01 12:02:13 +0300

    4-th

--- banking_system/banking_system.py
+++ banking_system/banking_system.py
@@ -307,3 +307,138 @@ while True:
         print("\nBye!")
         conn.close()
         break
+
+def check_luhn(card_number):
+    checksum = int(card_number[-1])
+    number = card_number[:-1]
+
+    digits = [int(d) for d in number]
+
+    for i in range(len(digits)):
+        if i % 2 == 0:
+            digits[i] *= 2
+            if digits[i] > 9:
+                digits[i] -= 9
+
+    total = sum(digits) + checksum
+
+    return total % 10 == 0
+
+while True:
+
+    print("1. Balance")
+    print("2. Add income")
+    print("3. Do transfer")
+    print("4. Close account")
+    print("5. Log out")
+    print("0. Exit")
+
+    account_choice = input()
+
+    if account_choice == "1":
+
+        cur.execute(
+            "SELECT balance FROM card WHERE number = ?",
+            (card_number,)
+        )
+
+        balance = cur.fetchone()[0]
+
+        print(f"\nBalance: {balance}\n")
+
+    elif account_choice == "2":
+
+        print("\nEnter income:")
+        income = int(input())
+
+        cur.execute("""
+        UPDATE card
+        SET balance = balance + ?
+        WHERE number = ?
+        """, (income, card_number))
+
+        conn.commit()
+
+        print("Income was added!\n")
+
+    elif account_choice == "3":
+
+        print("\nTransfer")
+        print("Enter card number:")
+
+        destination = input()
+
+        if destination == card_number:
+            print("\nYou can't transfer money to the same account!\n")
+            continue
+
+        if not check_luhn(destination):
+            print(
+                "\nProbably you made a mistake "
+                "in the card number. Please try again!\n"
+            )
+            continue
+
+        cur.execute(
+            "SELECT * FROM card WHERE number = ?",
+            (destination,)
+        )
+
+        receiver = cur.fetchone()
+
+        if receiver is None:
+            print("\nSuch a card does not exist.\n")
+            continue
+
+        print("Enter how much money you want to transfer:")
+        amount = int(input())
+
+        cur.execute(
+            "SELECT balance FROM card WHERE number = ?",
+            (card_number,)
+        )
+
+        balance = cur.fetchone()[0]
+
+        if amount > balance:
+            print("\nNot enough money!\n")
+            continue
+
+        cur.execute("""
+        UPDATE card
+        SET balance = balance - ?
+        WHERE number = ?
+        """, (amount, card_number))
+
+        cur.execute("""
+        UPDATE card
+        SET balance = balance + ?
+        WHERE number = ?
+        """, (amount, destination))
+
+        conn.commit()
+
+        print("\nSuccess!\n")
+
+    elif account_choice == "4":
+
+        cur.execute(
+            "DELETE FROM card WHERE number = ?",
+            (card_number,)
+        )
+
+        conn.commit()
+
+        print("\nThe account has been closed!\n")
+        break
+
+    elif account_choice == "5":
+
+        print("\nYou have successfully logged out!\n")
+        break
+
+    elif account_choice == "0":
+
+        print("\nBye!")
+        conn.close()
+        exit()
