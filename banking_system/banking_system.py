349b935be2f10682faea0f2ce2703c705a6fa542
BogdanMelenets <b.a.melenets@student.khai.edu>
2026-06-01 12:00:12 +0300

    3-rd

--- banking_system/banking_system.py
+++ banking_system/banking_system.py
@@ -169,3 +169,141 @@ while True:
     elif choice == "0":
         print("\nBye!")
         break
+
+import sqlite3
+import random
+
+
+conn = sqlite3.connect("card.s3db")
+cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS card (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    number TEXT,
+    pin TEXT,
+    balance INTEGER DEFAULT 0
+);
+""")
+
+conn.commit()
+
+
+def generate_pin():
+    return str(random.randint(0, 9999)).zfill(4)
+
+
+def luhn_checksum(number):
+    digits = [int(d) for d in number]
+
+    for i in range(len(digits)):
+        if i % 2 == 0:
+            digits[i] *= 2
+            if digits[i] > 9:
+                digits[i] -= 9
+
+    total = sum(digits)
+    return str((10 - total % 10) % 10)
+
+
+def generate_card_number():
+    while True:
+        account_identifier = str(
+            random.randint(0, 999999999)
+        ).zfill(9)
+
+        card_without_checksum = "400000" + account_identifier
+
+        card_number = (
+            card_without_checksum +
+            luhn_checksum(card_without_checksum)
+        )
+
+        cur.execute(
+            "SELECT number FROM card WHERE number = ?",
+            (card_number,)
+        )
+
+        if cur.fetchone() is None:
+            return card_number
+
+
+while True:
+    print("1. Create an account")
+    print("2. Log into the account")
+    print("0. Exit")
+
+    choice = input()
+
+    if choice == "1":
+
+        card_number = generate_card_number()
+        pin = generate_pin()
+
+        cur.execute("""
+        INSERT INTO card(number, pin)
+        VALUES(?, ?)
+        """, (card_number, pin))
+
+        conn.commit()
+
+        print("\nYour card has been created")
+        print("Your card number:")
+        print(card_number)
+        print("Your card PIN:")
+        print(pin)
+        print()
+
+    elif choice == "2":
+
+        print("\nEnter your card number:")
+        card_number = input()
+
+        print("Enter your PIN:")
+        pin = input()
+
+        cur.execute("""
+        SELECT * FROM card
+        WHERE number = ? AND pin = ?
+        """, (card_number, pin))
+
+        account = cur.fetchone()
+
+        if account:
+            print("\nYou have successfully logged in!\n")
+
+            while True:
+                print("1. Balance")
+                print("2. Log out")
+                print("0. Exit")
+
+                account_choice = input()
+
+                if account_choice == "1":
+
+                    cur.execute("""
+                    SELECT balance
+                    FROM card
+                    WHERE number = ?
+                    """, (card_number,))
+
+                    balance = cur.fetchone()[0]
+
+                    print(f"\nBalance: {balance}\n")
+
+                elif account_choice == "2":
+                    print("\nYou have successfully logged out!\n")
+                    break
+
+                elif account_choice == "0":
+                    print("\nBye!")
+                    conn.close()
+                    exit()
+
+        else:
+            print("\nWrong card number or PIN!\n")
+
+    elif choice == "0":
+        print("\nBye!")
+        conn.close()
+        break
