43edbad0356dc37ad92e7494a0f36f05563c3388
BogdanMelenets <b.a.melenets@student.khai.edu>
2026-06-01 11:41:59 +0300

    5-st

--- dominoes1/dominoes1.py
+++ dominoes1/dominoes1.py
@@ -163,3 +163,56 @@ def legal_move(piece, side):
             return temp
 
     return None
+
+# Перевірка ходу гравця:
+piece = player_pieces[abs(move)-1]
+
+if move > 0:
+
+    result = legal_move(piece, "right")
+
+    if result is None:
+        print("Illegal move. Please try again.")
+        continue
+
+    domino_snake.append(result)
+
+else:
+
+    result = legal_move(piece, "left")
+
+    if result is None:
+        print("Illegal move. Please try again.")
+        continue
+
+    domino_snake.insert(0, result)
+
+player_pieces.remove(piece)
+
+# Для комп'ютера:
+possible = list(range(-len(computer_pieces), len(computer_pieces)+1))
+random.shuffle(possible)
+
+for move in possible:
+
+    if move == 0:
+        continue
+
+    piece = computer_pieces[abs(move)-1]
+
+    if move > 0:
+        result = legal_move(piece, "right")
+        if result:
+            domino_snake.append(result)
+            computer_pieces.remove(piece)
+            break
+
+    else:
+        result = legal_move(piece, "left")
+        if result:
+            domino_snake.insert(0, result)
+            computer_pieces.remove(piece)
+            break
+else:
+    if stock_pieces:
+        computer_pieces.append(stock_pieces.pop())
